7ee603165f4261f80869a69080b1838abd91b9f8
Deepanshu <76985826+Deepanshu10123@users.noreply.github.com>
2026-08-04 22:37:21 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+        {
+            return false;
+        }
+        unordered_map<char, int> arr;
+        for(int i = 0; i<s.size(); i++)
+        {
+            arr[s[i]]++;
+            arr[t[i]]--;
+        }
+        for(auto i : arr)
+        {
+            if(i.second !=0)
+            return false;
+        }
+        return true;
+    }
+};
